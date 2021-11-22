987890165d2dc4bca29101466e252eed1989d110
unknown <>
2021-11-22 19:15:11 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CalendarEvent } from 'angular-calendar';
+import { CalendarView } from 'angular-calendar';
+import * as dateFns from "date-fns";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  
+  viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Month;
+  mCalendarView = CalendarView;
+events: CalendarEvent[] = [
+    {
+      start: dateFns.startOfDay(new Date("11/26/2021")),
+      title: 'An event with no end date',
+    }
+  ]
+  
+dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+  console.log(date);
+  console.log( (date.getMonth() + 1 )+ "/" + date.getDate() + "/" + date.getFullYear())
+  events.push({
+    start: new Date( date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear()),
+    title:"patient_name",
+  });
+  // https://javascript.plainenglish.io/create-calendar-using-angular-and-bootstrap-monthly-weekly-and-daily-calendar-c441f1cb8b18
+//   this.events = [
+//     ...this.events,
+//     {
+// start: new Date( date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear()),
+// title:"patient_name"
+// }
+// ]
+  //this.openAppointmentList(date)
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter, CalendarEvent } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+BrowserModule,
+    AppRoutingModule,
+    
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
